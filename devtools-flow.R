e2899106b9b0b6e7626bd8999b494c5a9aabf974
mklarson <marykathrynlarson@berkeley.edu>
2018-12-02 22:27:44 -0800

    description and namespace

--- devtools-flow.R
+++ devtools-flow.R
@@ -1,1 +1,11 @@
-devtools-flow.R
+# =====================================================
+# Devtools workflow
+#library(devtools)
+# =====================================================
+
+devtools::document()          # generate documentation
+devtools::check_man()         # check documentation
+devtools::test()              # run tests
+devtools::build_vignettes()   # build vignettes
+devtools::build()             # build bundle
+devtools::install()           # install package
